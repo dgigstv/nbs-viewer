d37969b2d14cf1a6ecf573860c68f64ba4c92371
dgigstv <dgigstv@protonmail.com>
2022-09-04 17:50:58 -0400

    #1: Remove deleted package. Bump package versions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,15 @@
 module.exports = {
-    extends: './node_modules/dgigs-lint/typescript',
-    parserOptions: {
-        project: 'tsconfig.lint.json',
-    },
-};
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    project: ['tsconfig.lint.json'],
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  root: true,
+}
